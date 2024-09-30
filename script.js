6a9002dc9fcca0dafb7a0818c9a9a5905bfbb7c4
JustHumanoid <78427090+JustHumanoid@users.noreply.github.com>
2024-09-30 11:55:42 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple JavaScript for the buttons
+function shopNow() {
+    window.location.href = "#shop";
+}
+
+function addToCart(product) {
+    alert(product + " has been added to your cart!");
+}
