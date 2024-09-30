9fdbda375f782b244cb881dfe3e10729b677ac35
JustHumanoid <78427090+JustHumanoid@users.noreply.github.com>
2024-09-30 12:00:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-// Simple JavaScript for the buttons
+// Scroll to the shop section
 function shopNow() {
-    window.location.href = "#shop";
+    document.getElementById('shop').scrollIntoView({ behavior: 'smooth' });
 }
 
-function addToCart(product) {
-    alert(product + " has been added to your cart!");
+// Open modal with product details
+function openModal(product, price) {
+    document.getElementById('productModal').style.display = 'flex';
+    document.getElementById('modalTitle').innerText = product;
+    document.getElementById('modalPrice').innerText = "$" + price + ".00";
+    document.getElementById('addToCartBtn').setAttribute('data-product', product);
+}
+
+// Close modal
+function closeModal() {
+    document.getElementById('productModal').style.display = 'none';
+}
+
+// Add product to cart
+function addToCart() {
+    const product = document.getElementById('addToCartBtn').getAttribute('data-product');
+    alert(product + ' has been added to your cart!');
+    closeModal();
 }
