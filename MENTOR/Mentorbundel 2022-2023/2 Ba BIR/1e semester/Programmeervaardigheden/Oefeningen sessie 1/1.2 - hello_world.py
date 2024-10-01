778627860aa7d25e6511ef1c616ccd82fe1b4db6
JustHumanoid <78427090+JustHumanoid@users.noreply.github.com>
2024-10-01 15:28:29 +0200

    Delete MENTOR/Mentorbundel 2022-2023 directory

--- MENTOR/Mentorbundel 2022-2023/2 Ba BIR/1e semester/Programmeervaardigheden/Oefeningen sessie 1/1.2 - hello_world.py
+++
@@ -1,3 +0,0 @@
-print "Hello world!"
-import time
-time.sleep(5)
--- MENTOR/Mentorbundel 2022-2023/2 Ba BIR/1e semester/Programmeervaardigheden/Oefeningen sessie 1/1.6 - sinusx.py
+++
@@ -1,6 +0,0 @@
-from math import sin
-x= input ('geef waarde tussen 0 en pi')
-print 'sin(%g)=%g' % (x, sin(x))
-
-import time
-time.sleep(3)
